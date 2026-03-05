80bb26515357b052a102a178614d3375f276c8fc
2sh <git@2sh.me>
2026-03-05 14:04:33 +0000

    Move footer to bottom of page

--- src/App.vue
+++ src/App.vue
@@ -2,5 +2,13 @@
 <template>
 <main>
   <RouterView />
+  <div id="footer" lang="en">
+    <div></div>
+    <div>
+      <span class="no-wrap">By <a href="https://2sh.me/gothic">2sh</a> (2026).</span> <span
+        class="no-wrap">Weather & Geolocation data by <a href="https://open-meteo.com/">Open-Meteo</a>.</span>
+      <br>Any location data is sent directly from your browser to Open-Meteo only.
+    </div>
+  </div>
 </main>
 </template>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -575,14 +575,6 @@ const options = ref({
       </div>
     </div>
   </div>
-  <div id="footer" lang="en">
-    <div></div>
-    <div>
-      <span class="no-wrap">By <a href="https://2sh.me/gothic">2sh</a> (2026).</span> <span
-        class="no-wrap">Weather & Geolocation data by <a href="https://open-meteo.com/">Open-Meteo</a>.</span>
-      <br>Any location data is sent directly from your browser to Open-Meteo only.
-    </div>
-  </div>
   <Search v-if="showSearch" @close="showSearch=false" @set="handleSetSearch"></Search>
   <Options v-if="showOptions" v-model="options" @close="handleCloseOptions"></Options>
 </div>
